7e30023d0018147139807fce5c39b9f8dde615b6
Python-Alex <52885126+Python-Alex@users.noreply.github.com>
2024-11-05 18:41:23 -0800

    added_test

---
+++ test.lua
@@ -0,0 +1,9 @@
+engine_controller = require("controllers.engine_controller")
+fuel_controller = require("controllers.fuel_controller")
+redstone_controller = require("controllers.redstone_controller")
+engine_sensor = require("sensors.engine_sensors")
+
+print("Engine lib: " .. tostring(engine_controller))
+print("Fuel lib: " .. tostring(fuel_controller))
+print("Redstone lib: " .. tostring(redstone_controller))
+
